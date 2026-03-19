f3f19d2eeb4005f2f6c002fcbb59c93f9ff445b6
ruoqizhao1idm <zrqqqq123@163.com>
2026-03-20 05:23:37 +0800

    refine login and register controllers

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -10,10 +10,46 @@ class UserController {
     console.log(user[0].username);
 
     //You can now send these records to your template
-    res.render("user/", {
+    res.render("user/index", {
         userInfo : user,
         userPosts : posts
     });
+  }
+  //login
+  showLogin(req, res) {
+    res.render("user/login", { error: null }); 
+  }
+  async login(req, res) {
+    const { login, password } = req.body; // 'login' matches the name="login" in your EJS
+    const user = await usermodel.findUserByUsername(login);
+
+    if (user && user.password === password) {
+        // Simple authentication: save user object to session
+        req.session.user = user;
+        res.redirect("/homepage");
+    } else {
+        res.render("user/login", { error: "Invalid username or password" });
+    }
+  }
+
+  //register
+  showRegister(req, res) {
+    res.render("user/register", { error: null });
+  }
+  async register(req, res) {
+    const { username, password } = req.body;
+    try {
+        await usermodel.registerUser(username, password);
+        res.redirect("/login");
+    } catch (err) {
+        res.render("user/register", { error: "Registration failed or username taken" });
+    }
+  }
+
+  //logout
+  logout(req, res) {
+    req.session.destroy();
+    res.redirect("/login");
   }
 }
 
